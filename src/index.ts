14b742725a11901572ed86b567084da331c4233d
Chang Woo Park <whwh3560@gmail.com>
2023-09-27 18:56:46 +0900

    feat: [front] output test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-// ! 최종 아웃풋 파일 
+import NextFetch from "./core/WooFetch";
+
+interface CreateProps {
+  baseUrl: string;
+  timeOut?: number;
+  header?: any;
+}
+
+// ! 추후 좀 더 세부적으로 셋팅 필요
+const defaultConfig = {
+  baseUrl: '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  //! AbortSignal.timeout 에러 있을 수 있으므로 timeout 감싼 new AbortController().signal 로 처리
+  signal: AbortSignal.timeout(8000),
+};
+
+function CreateInstance(config: typeof defaultConfig) {
+  const instance = new NextFetch(config);
+
+  // instance.create = (instanceConfig: CreateProps) => {
+  //   return CreateInstance({ ...config, ...instanceConfig });
+  // };
+
+  return instance;
+}
+
+const nextFetch = CreateInstance(defaultConfig);
+
+export default nextFetch;
+
+//! 자동완성과 타입 추론을 못하는 에러 발생
