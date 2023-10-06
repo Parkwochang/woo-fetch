c8fabacc2cdd1177eb581985de36b54466720251
Chang Woo Park <whwh3560@gmail.com>
2023-10-06 21:25:27 +0900

    chore: [root] tsup -> remove cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig, Options } from "tsup";
 
+const env = process.env.NODE_ENV;
+
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
   entry: ["src/**/*.ts"],
-  format: ["esm", "cjs"],
+  entryPoints: ['src/index.ts'],
+  format: ["esm"],
   dts: true,
   minify: true,
   clean: true,
-  // external: ["react"],
+  target: "es2020",
   ...options,
 }));
+
+// "cjs"
+
+// export const tsup: Options = {
+//   splitting: true,
+//   clean: true, // clean up the dist folder
+//   // generate dts files
+//   format: ['cjs', 'esm'], // generate cjs and esm files
+//   minify: env === 'production',
+//   bundle: env === 'production',
+//   skipNodeModulesBundle: true,
+
+//   watch: env === 'development',
+//   target: 'es2020',
+//   outDir: env === 'production' ? 'dist' : 'lib',
+//   entry: ['src/**/*.ts'], //include all files under src
+// };
